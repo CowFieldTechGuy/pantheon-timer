71e31ae241e0468de00e7a0e128e46482e34d9b9
Chris <chris@Mac.localdomain>
2025-02-25 17:49:40 -0600

    Fixed styling issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Make sure this line is here
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// rest of the file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
